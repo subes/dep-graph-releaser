5b8e43847510fbd84d50c078da90acea56a4884c
Robert Stoll <robert.stoll@loewenfels.ch>
2018-02-07 13:54:13 +0100

    a release plan consists of multiple Projects not Commands

--- dep-graph-releaser-api/src/main/kotlin/ch/loewenfels/depgraph/data/ReleasePlan.kt
+++ dep-graph-releaser-api/src/main/kotlin/ch/loewenfels/depgraph/data/ReleasePlan.kt
@@ -1,3 +1,7 @@
 package ch.loewenfels.depgraph.data
 
-data class ReleasePlan(val commands: List<Command>)
+/**
+ * Represents a release plan whereas the first [Project] in [projects] is the root project,
+ * the one which should be released, whereas the others are dependent projects.
+ */
+data class ReleasePlan(val projects: List<Project>)
