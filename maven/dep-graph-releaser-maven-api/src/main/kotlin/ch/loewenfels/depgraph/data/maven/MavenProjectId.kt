03f50ffcef9e2b1d3bb483aba45efe0a46592775
Robert Stoll <robert.stoll@loewenfels.ch>
2018-02-12 09:32:02 +0100

    remove packaging and classifier from MavenProjectId
    - describe the reason in KDoc

--- maven/dep-graph-releaser-maven-api/src/main/kotlin/ch/loewenfels/depgraph/data/maven/MavenProjectId.kt
+++ maven/dep-graph-releaser-maven-api/src/main/kotlin/ch/loewenfels/depgraph/data/maven/MavenProjectId.kt
@@ -3,25 +3,26 @@ package ch.loewenfels.depgraph.data.maven
 import ch.loewenfels.depgraph.data.ProjectId
 
 /**
- * Represents a [ProjectId] for maven projects composed by [groupId]:[artifactId]:[packaging]:[classifier].
+ * Represents a [ProjectId] for maven projects composed by [groupId]:[artifactId] and its [version].
+ *
+ * `packaging` and `classifier` is neglected on purpose. For instance, having a tests jar is seen as an additional
+ * artifact during a release. We assume that such an artifact is never released alone but always together with all other
+ * artifacts (otherwise it would have been released under a different artifactId and not just distinguished in its
+ * classifier). Open an issue if you require it.
  *
  * Have a look at [Apache's Documentation](https://maven.apache.org/pom.html#Maven_Coordinates) for further information.
  *
  * @param groupId The `groupId of the maven project.
  * @param artifactId The artifactId of the maven project.
- * @param packaging the packaging of the maven project; "jar" per default.
- * @param classifier An optional classifier of the maven project; empty per default which means none.
  */
 data class MavenProjectId(
     val groupId: String,
     val artifactId: String,
-    override val version: String,
-    val packaging: String = "jar",
-    val classifier: String = "") : ProjectId {
+    override val version: String) : ProjectId {
 
     /**
-     * Returns [groupId]:[artifactId]:[packaging]:[classifier]
+     * Returns [groupId]:[artifactId]
      * @return The identifier of the maven artifact (without its [version])
      */
-    override val identifier = "$groupId:$artifactId:$packaging:$classifier"
+    override val identifier = "$groupId:$artifactId"
 }
