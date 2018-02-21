ee6e93688a2246e814df4696373f5e183fc17493
Robert Stoll <robert.stoll@loewenfels.ch>
2018-02-21 14:42:33 +0100

    introduce html GUI
    - use kotlinx.html
    - output all projects for the moment (no lines between projects yet or
      any other ordering)

---
+++ dep-graph-releaser-html/src/main/kotlin/ch/loewenfels/depgraph/html/ReleasePlanToHtml.kt
@@ -0,0 +1,228 @@
+package ch.loewenfels.depgraph.html
+
+import ch.loewenfels.depgraph.data.*
+import ch.loewenfels.depgraph.data.maven.MavenProjectId
+import ch.loewenfels.depgraph.data.maven.jenkins.JenkinsMavenReleasePlugin
+import ch.loewenfels.depgraph.data.maven.jenkins.JenkinsUpdateDependency
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
+
+class ReleasePlanToHtml {
+    fun createHtml(releasePlan: ReleasePlan): StringBuilder {
+        val sb = StringBuilder()
+        sb.appendHTML().html {
+            head(releasePlan)
+            body(releasePlan)
+        }
+        return sb
+    }
+
+    private fun HTML.body(releasePlan: ReleasePlan) {
+        body {
+            val projectsToVisit = mutableListOf(releasePlan.rootProjectId)
+            val visitedProjects = hashSetOf<ProjectId>()
+            while (projectsToVisit.isNotEmpty()) {
+                val projectId = projectsToVisit.removeAt(0)
+                val project = releasePlan.getProject(projectId)
+                if (!visitedProjects.contains(projectId)) {
+                    project(project)
+                    projectsToVisit.addAll(releasePlan.getDependents(projectId))
+                }
+            }
+
+        }
+    }
+
+    private fun BODY.project(project: Project) {
+        val id = project.id
+        div("project") {
+            div("title") {
+                toggle("${id.identifier}:disableAll", project.commands.any { it.state !is CommandState.Deactivated })
+                span {
+                    if (id is MavenProjectId) {
+                        title = id.identifier
+                        +id.artifactId
+                    } else {
+                        +id.identifier
+                    }
+                }
+
+            }
+            div("fields") {
+                fieldReadOnlyWithLabel("${id.identifier}:currentVersion", "Current Version", project.currentVersion)
+                fieldWithLabel("${id.identifier}:releaseVersion", "Release Version", project.releaseVersion)
+            }
+            commands(project)
+        }
+    }
+
+    private fun DIV.commands(project: Project) {
+        project.commands.forEachIndexed { index, command ->
+            div {
+                classes = setOf("command", stateToCssClass(command.state))
+                div("commandTitle") { +command::class.java.simpleName }
+                div("fields") {
+                    fieldsForCommand("${project.id.identifier}:$index:", command)
+                }
+            }
+        }
+    }
+
+    private fun stateToCssClass(state: CommandState) = when (state) {
+        is CommandState.Waiting -> "waiting"
+        CommandState.Ready -> "ready"
+        CommandState.InProgress -> "inProgress"
+        CommandState.Succeeded -> "succeeded"
+        is CommandState.Failed -> "failed"
+        is CommandState.Deactivated -> "deactivated"
+    }
+
+    private fun DIV.fieldWithLabel(id: String, label: String, text: String) {
+        fieldWithLabel(id, label, text, false)
+    }
+
+    private fun DIV.fieldReadOnlyWithLabel(id: String, label: String, text: String) {
+        fieldWithLabel(id, label, text, true)
+    }
+
+    private fun DIV.fieldWithLabel(id: String, label: String, text: String, disabled: Boolean) {
+        div {
+            label("fields") {
+                htmlFor = id
+                +label
+            }
+            input {
+                type = InputType.text
+                this.disabled = disabled
+                this.id = id
+                value = text
+            }
+        }
+    }
+
+
+    private fun DIV.fieldsForCommand(idPrefix: String, command: Command) {
+        toggle("$idPrefix:disable", command.state !is CommandState.Deactivated)
+        when (command) {
+            is JenkinsMavenReleasePlugin -> {
+                fieldWithLabel("$idPrefix:nextDevVersion", "Next Dev Version", command.nextDevVersion)
+            }
+            is JenkinsUpdateDependency -> {
+                fieldReadOnlyWithLabel("$idPrefix:groupId", "Dependency", command.projectId.identifier)
+            }
+        }
+
+    }
+
+    private fun DIV.toggle(id: String, checked: Boolean) {
+        label("toggle") {
+            input {
+                type = InputType.checkBox
+                this.id = id
+                this.checked = checked
+            }
+            span("slider")
+        }
+    }
+
+    private fun HTML.head(releasePlan: ReleasePlan) {
+        head {
+            title {
+                +"Release ${releasePlan.rootProjectId.identifier}"
+            }
+            style {
+                unsafe {
+                    raw("""
+     body {
+	    font-family: "Arimo", sans-serif;
+		font-size:12px;
+	}
+	div.project {
+		border:1px solid #CCC;
+		background: #fff;
+		margin: 20px auto;
+		display: block;
+		width: 300px;
+		padding:20px 15px;
+		border-radius: 2px 2px 2px 2px;
+		-webkit-box-shadow: 0 1px 4px
+		rgba(0, 0, 0, 0.3), 0 0 40px
+		rgba(0, 0, 0, 0.1) inset;
+		-moz-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+		box-shadow: 0 1px 4px
+		rgba(0, 0, 0, 0.3), 0 0 40px
+		rgba(0, 0, 0, 0.1) inset;
+	}
+	div.title {
+		font-size:1.2em;
+		font-weight: bold;
+		margin-bottom:5px;
+	}
+	div.commandTitle {
+		font-weight:bold;
+		margin-top:10px;
+		margin-bottom: 3px;
+	}
+	label.fields {
+		display: inline-block;
+		width: 100px;
+		text-align: right;
+		padding-right: 6px;
+	}
+	input {
+		width:150px;
+	}
+    label.toggle {
+		position: relative;
+		display: inline-block;
+		width: 35px;
+		height: 20px;
+		float:right;
+	}
+	label.toggle input {
+		display:none;
+	}
+	label.toggle .slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #ccc;
+		-webkit-transition: .4s;
+		transition: .4s;
+		border-radius: 34px;
+	}
+	label.toggle .slider:before {
+	  position: absolute;
+	  content: "";
+	  height: 12px;
+	  width: 12px;
+	  left: 4px;
+	  bottom: 4px;
+	  background-color: white;
+	  -webkit-transition: .4s;
+	  transition: .4s;
+	  border-radius: 50%;
+	}
+
+	label.toggle input:checked + .slider {
+	  background-color: #2196F3;
+	}
+	input:focus + .slider {
+	  box-shadow: 0 0 1px #2196F3;
+	}
+
+	input:checked + .slider:before {
+	  -webkit-transform: translateX(14px);
+	  -ms-transform: translateX(14px);
+	  transform: translateX(14px);
+	}
+""")
+                }
+            }
+        }
+    }
+}
+
---
+++ dep-graph-releaser-html/src/test/kotlin/ch/loewenfels/depgraph/html/ReleasePlanToHtmlSpec.kt
@@ -0,0 +1,68 @@
+package ch.loewenfels.depgraph.html
+
+import ch.loewenfels.depgraph.data.CommandState
+import ch.loewenfels.depgraph.data.Project
+import ch.loewenfels.depgraph.data.ProjectId
+import ch.loewenfels.depgraph.data.ReleasePlan
+import ch.loewenfels.depgraph.data.maven.MavenProjectId
+import ch.loewenfels.depgraph.data.maven.jenkins.JenkinsMavenReleasePlugin
+import ch.loewenfels.depgraph.data.maven.jenkins.JenkinsUpdateDependency
+import ch.tutteli.atrium.api.cc.en_UK.contains
+import ch.tutteli.atrium.assert
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+import java.io.File
+import java.io.FileWriter
+
+object ReleasePlanToHtmlSpec : Spek({
+
+    val rootProjectId = MavenProjectId("com.example", "a")
+    val rootProject = Project(rootProjectId, "1.1.0-SNAPSHOT", "1.2.0", listOf(JenkinsMavenReleasePlugin(CommandState.Ready, "1.2.1-SNAPSHOT")))
+    val projectWithDependentId = MavenProjectId("com.example", "b")
+    val projectWithDependentUpdateDependency = JenkinsUpdateDependency(CommandState.Waiting(setOf(rootProjectId)), rootProjectId)
+    val projectWithDependentJenkinsRelease = JenkinsMavenReleasePlugin(CommandState.Waiting(setOf(rootProjectId)), "3.1-SNAPSHOT")
+    val projectWithDependentCommands = listOf(
+        projectWithDependentUpdateDependency,
+        projectWithDependentJenkinsRelease
+    )
+    val projectWithDependent = Project(projectWithDependentId, "2.0", "3.0", projectWithDependentCommands)
+
+    val projectWithoutDependentId = MavenProjectId("com.example", "c")
+    val projectWithoutDependentUpdateDependency1 = JenkinsUpdateDependency(CommandState.Waiting(setOf(rootProjectId)), rootProjectId)
+    val projectWithoutDependentUpdateDependency2 = JenkinsUpdateDependency(CommandState.Waiting(setOf(projectWithDependentId)), projectWithDependentId)
+    val projectWithoutDependentJenkinsRelease = JenkinsMavenReleasePlugin(CommandState.Deactivated(CommandState.Waiting(setOf(projectWithDependentId, rootProjectId))), "4.2-SNAPSHOT")
+    val projectWithoutDependentCommands = listOf(
+        projectWithoutDependentUpdateDependency1,
+        projectWithoutDependentUpdateDependency2,
+        projectWithoutDependentJenkinsRelease
+    )
+    val projectWithoutDependent = Project(projectWithoutDependentId, "4.0", "4.1", projectWithoutDependentCommands)
+
+    val dependents = mapOf<ProjectId, Set<MavenProjectId>>(
+        rootProjectId to setOf(projectWithDependentId),
+        projectWithDependentId to setOf(projectWithoutDependentId),
+        projectWithoutDependentId to setOf()
+    )
+    val releasePlan = ReleasePlan(rootProjectId,
+        mapOf(
+            rootProjectId to rootProject,
+            projectWithDependentId to projectWithDependent,
+            projectWithoutDependentId to projectWithoutDependent
+        ),
+        dependents
+    )
+    val testee = ReleasePlanToHtml()
+
+    action("describe release plan to HTML smoke tests") {
+        val result = testee.createHtml(releasePlan).toString()
+        FileWriter(File("test.html")).use {
+            it.write(result)
+        }
+
+        it("contains all projects") {
+            val keys = releasePlan.projects.keys.map { """title="${it.identifier}"""" }
+            assert(result).contains(keys[0], *keys.subList(1, keys.size).toTypedArray())
+        }
+    }
+
+})
