8a41637b30b998835460c6b7999201afa904dc08
Robert Stoll <robert.stoll@loewenfels.ch>
2018-04-03 16:22:36 +0200

    update parent relation
    - implement updating parent relations, reuse the logic for updating
      dependencies
    - write spec

--- dep-graph-releaser-manipulation/src/main/kotlin/ch/loewenfels/depgraph/RegexBasedVersionUpdater.kt
+++ dep-graph-releaser-manipulation/src/main/kotlin/ch/loewenfels/depgraph/RegexBasedVersionUpdater.kt
@@ -11,28 +11,54 @@ class RegexBasedVersionUpdater {
             Regex("(?:$groupIdPattern[\\S\\s]*?$artifactIdPattern)|(?:$artifactIdPattern[\\S\\s]*?$groupIdPattern)")
         val content = pom.readText()
 
-        val paramObject = ParamObject(groupId, artifactId, newVersion, content, dependencyRegex.find(content, 0))
-        while (paramObject.matchResult != null) {
-            val matchResult = paramObject.matchResult!!
-            paramObject.appendSubstring(paramObject.startIndex, matchResult.range.start)
-            paramObject.startIndex = matchResult.range.start
-            if (groupIdArtifactIdRegex.containsMatchIn(matchResult.value)) {
-                appendDependency(paramObject)
-            }
-            paramObject.matchResult = matchResult.next()
-        }
-        paramObject.appendSubstring(paramObject.startIndex)
+        val dependenciesParamObject = ParamObject(groupId, artifactId, newVersion, content, dependencyRegex)
+        updateDependencies(dependenciesParamObject, groupIdArtifactIdRegex)
+
+        val parentParamObject = ParamObject(dependenciesParamObject, parentRegex)
+        updateParentRelation(parentParamObject, groupIdArtifactIdRegex, pom)
 
         //TODO deal with properties
 
-        if (paramObject.updated) {
-            pom.writeText(paramObject.modifiedPom.toString())
+        if (dependenciesParamObject.updated || parentParamObject.updated) {
+            pom.writeText(parentParamObject.getNewContent())
         } else {
             throw IllegalStateException(
-                "cannot update dependency $groupId:$artifactId because " +
+                "cannot update (parent) dependency $groupId:$artifactId because " +
                     "it is either not managed by the given pom or " +
                     "not in there at all.\npom: ${pom.absolutePath}"
             )
+        }
+    }
+
+    private fun updateParentRelation(
+        parentParamObject: ParamObject,
+        groupIdArtifactIdRegex: Regex,
+        pom: File
+    ) {
+        val matchResult = parentParamObject.matchResult
+        if (matchResult != null && groupIdArtifactIdRegex.containsMatchIn(matchResult.value)) {
+            parentParamObject.appendBeforeMatchAndUpdateStartIndex()
+            appendDependency(parentParamObject)
+            check(matchResult.next() == null) {
+                "pom has two <parent> -- file: ${pom.absolutePath}"
+            }
+        }
+        if (parentParamObject.updated) {
+            parentParamObject.appendSubstring(parentParamObject.startIndex)
+        }
+    }
+
+    private fun updateDependencies(dependenciesParamObject: ParamObject, groupIdArtifactIdRegex: Regex) {
+        while (dependenciesParamObject.matchResult != null) {
+            val matchResult = dependenciesParamObject.matchResult!!
+            if (groupIdArtifactIdRegex.containsMatchIn(matchResult.value)) {
+                dependenciesParamObject.appendBeforeMatchAndUpdateStartIndex()
+                appendDependency(dependenciesParamObject)
+            }
+            dependenciesParamObject.matchResult = matchResult.next()
+        }
+        if (dependenciesParamObject.updated) {
+            dependenciesParamObject.appendSubstring(dependenciesParamObject.startIndex)
         }
     }
 
@@ -80,12 +106,30 @@ class RegexBasedVersionUpdater {
         val artifactId: String,
         val newVersion: String,
         private val content: String,
-        var matchResult: MatchResult?
+        regex: Regex
     ) {
+        /**
+         * Copy constructor where [getNewContent] is used as new [content] and [newRegex] as [newRegex]
+         */
+        constructor(paramObject: ParamObject, newRegex: Regex) : this(
+            paramObject.groupId,
+            paramObject.artifactId,
+            paramObject.newVersion,
+            paramObject.getNewContent(),
+            newRegex
+        )
+
         val modifiedPom = StringBuilder()
+        var matchResult = regex.find(content, 0)
         var startIndex: Int = 0
         val properties = hashSetOf<String>()
         var updated = false
+
+        fun appendBeforeMatchAndUpdateStartIndex(){
+            val nonNullMatchResult = matchResult!!
+            appendSubstring(startIndex, nonNullMatchResult.range.start)
+            startIndex = nonNullMatchResult.range.start
+        }
 
         fun appendSubstring(startIndex: Int) {
             modifiedPom.append(content.substring(startIndex))
@@ -94,10 +138,13 @@ class RegexBasedVersionUpdater {
         fun appendSubstring(startIndex: Int, endIndex: Int) {
             modifiedPom.append(content.substring(startIndex, endIndex))
         }
+
+        fun getNewContent() = if (updated) modifiedPom.toString() else content
     }
 
     companion object {
         private val dependencyRegex = Regex("<dependency>[\\S\\s]+?</dependency>")
+        private val parentRegex = Regex("<parent>[\\S\\s]+?</parent>")
         private val versionRegex = Regex("<version>([^<]+)</version>")
         private val propertyRegex = Regex("\\$\\{[^}]+}")
     }
--- dep-graph-releaser-manipulation/src/test/kotlin/ch/loewenfels/depgraph/RegexBasedVersionUpdaterSpec.kt
+++ dep-graph-releaser-manipulation/src/test/kotlin/ch/loewenfels/depgraph/RegexBasedVersionUpdaterSpec.kt
@@ -1,6 +1,7 @@
 package ch.loewenfels.depgraph
 
 import ch.loewenfels.depgraph.maven.getTestDirectory
+import ch.tutteli.atrium.IdAndVersions
 import ch.tutteli.atrium.api.cc.en_UK.containsRegex
 import ch.tutteli.atrium.api.cc.en_UK.toBe
 import ch.tutteli.atrium.api.cc.en_UK.toThrow
@@ -9,10 +10,7 @@ import ch.tutteli.atrium.exampleA
 import ch.tutteli.atrium.expect
 import ch.tutteli.spek.extensions.TempFolder
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.context
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.*
 import java.io.File
 
 object RegexBasedVersionUpdaterSpec : Spek({
@@ -41,12 +39,7 @@ object RegexBasedVersionUpdaterSpec : Spek({
         val pom = File(getTestDirectory("singleProject"), "pom.xml")
 
         context("dependency shall be updated, same version") {
-            it("updates the dependency and file content is the same as before") {
-                val tmpPom = tempFolder.newFile("pom.xml")
-                tmpPom.writeBytes(pom.readBytes())
-                testee.updateDependency(tmpPom, "junit", "junit", "4.12")
-                assert(tmpPom.readText()).toBe(pom.readText())
-            }
+            testSameContent(testee, tempFolder, pom, "junit", "junit", "4.12")
         }
 
         context("dependency shall be updated, new version") {
@@ -119,27 +112,97 @@ object RegexBasedVersionUpdaterSpec : Spek({
         val pom = File(getTestDirectory("managingVersions/viaDependencyManagement"), "b.pom")
 
         context("dependency shall be updated, same version") {
-            it("updates the dependency and file content is the same as before") {
-                val tmpPom = tempFolder.newFile("pom.xml")
-                tmpPom.writeBytes(pom.readBytes())
-                testee.updateDependency(tmpPom, exampleA.id.groupId, exampleA.id.artifactId, "1.0.0")
-                assert(tmpPom.readText()).toBe(pom.readText())
-            }
+            testSameContent(testee, tempFolder, pom, exampleA)
         }
 
         context("dependency shall be updated, new version") {
             it("updates the dependency") {
                 val tmpPom = tempFolder.newFile("pom.xml")
                 tmpPom.writeBytes(pom.readBytes())
-                testee.updateDependency(tmpPom,exampleA.id.groupId, exampleA.id.artifactId, "1.1.0")
+
+                updateDependency(testee, tmpPom, exampleA)
                 assert(tmpPom.readText()).containsRegex(
                     "<dependency>[\\S\\s]*" +
                         "<groupId>${exampleA.id.groupId}</groupId>[\\S\\s]*" +
                         "<artifactId>${exampleA.id.artifactId}</artifactId>[\\S\\s]*" +
-                        "<version>1.1.0</version>[\\S\\s]*" +
+                        "<version>${exampleA.releaseVersion}</version>[\\S\\s]*" +
                         "</dependency>"
                 )
             }
         }
     }
+
+    given("project with parent dependency") {
+        val pom = File(getTestDirectory("parentRelations/parent"), "b.pom")
+
+        context("parent dependency shall be updated, same version") {
+            testSameContent(testee, tempFolder, pom, exampleA)
+        }
+
+        context("parent dependency shall be updated, new version") {
+            it("updates the dependency") {
+                val tmpPom = tempFolder.newFile("pom.xml")
+                tmpPom.writeBytes(pom.readBytes())
+                updateDependency(testee, tmpPom, exampleA)
+                assert(tmpPom.readText()).containsRegex(
+                    "<parent>[\\S\\s]*" +
+                        "<groupId>${exampleA.id.groupId}</groupId>[\\S\\s]*" +
+                        "<artifactId>${exampleA.id.artifactId}</artifactId>[\\S\\s]*" +
+                        "<version>${exampleA.releaseVersion}</version>[\\S\\s]*" +
+                        "</parent>"
+                )
+            }
+        }
+    }
 })
+
+private fun SpecBody.testSameContent(
+    testee: RegexBasedVersionUpdater,
+    tempFolder: TempFolder,
+    pom: File,
+    groupId: String,
+    artifactId: String,
+    newVersion: String
+) {
+    testSameContent(tempFolder, pom) { tmpPom ->
+        testee.updateDependency(tmpPom, groupId, artifactId, newVersion)
+    }
+}
+
+private fun SpecBody.testSameContent(
+    testee: RegexBasedVersionUpdater,
+    tempFolder: TempFolder,
+    pom: File,
+    idAndVersions: IdAndVersions
+) {
+    testSameContent(tempFolder, pom) { tmpPom ->
+        updateDependency(testee, tmpPom, idAndVersions, "1.0.0")
+    }
+}
+
+private fun SpecBody.testSameContent(
+    tempFolder: TempFolder,
+    pom: File,
+    update: (File) -> Unit
+) {
+    it("updates the dependency and file content is the same as before") {
+        val tmpPom = tempFolder.newFile("pom.xml")
+        tmpPom.writeBytes(pom.readBytes())
+        update(tmpPom)
+        assert(tmpPom.readText()).toBe(pom.readText())
+    }
+}
+
+private fun updateDependency(testee: RegexBasedVersionUpdater, tmpPom: File, project: IdAndVersions) {
+    updateDependency(testee, tmpPom, project, project.releaseVersion)
+}
+
+private fun updateDependency(
+    testee: RegexBasedVersionUpdater,
+    tmpPom: File,
+    project: IdAndVersions,
+    oldVersion: String
+) {
+    testee.updateDependency(tmpPom, project.id.groupId, project.id.artifactId, oldVersion)
+}
+
