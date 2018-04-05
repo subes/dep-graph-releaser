f3536e5e3e44a523fade3db3f7e26f6b85be5935
Robert Stoll <robert.stoll@loewenfels.ch>
2018-04-05 16:19:43 +0200

    introduce RemoteJenkinsM2Releaser

---
+++ dep-graph-releaser-jenkins/src/main/kotlin/ch/loewenfels/depgraph/jenkins/RemoteJenkinsM2Releaser.kt
@@ -0,0 +1,196 @@
+package ch.loewenfels.depgraph.jenkins
+
+import okhttp3.*
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+
+/**
+ * Used to create a release of an artifact with the m2release plugin on a remote jenkins host.
+ * It basically triggers the job via the REST API and polls it to see if it completed
+ */
+class RemoteJenkinsM2Releaser(
+    private val httpClient: OkHttpClient,
+    jenkinsBaseUrl: String,
+    private val jenkinsUsername: String,
+    private val jenkinsPassword: String,
+    private val maxTriggerTries: Int,
+    private val maxReleaseTimeInSeconds: Int,
+    private val poolEverySecond: Int,
+    private val parameters: Map<String, String>
+) {
+    private val jenkinsBaseUrl: String = if (jenkinsBaseUrl.endsWith("/")) {
+        jenkinsBaseUrl.substring(0, jenkinsBaseUrl.length - 1)
+    } else {
+        jenkinsBaseUrl
+    }
+
+    init {
+        require(jenkinsBaseUrl.startsWith("http")) {
+            "jenkinsBaseUrl does not start with http: $jenkinsBaseUrl"
+        }
+        require(jenkinsUsername.isNotBlank()) {
+            "jenkinsUsername was blank"
+        }
+        require(jenkinsPassword.isNotBlank()) {
+            "jenkinsPassword was blank"
+        }
+        require(maxTriggerTries > 0) {
+            "maxTriggerTries has to be greater than 0, given: $maxReleaseTimeInSeconds"
+        }
+        require(maxReleaseTimeInSeconds > 0) {
+            "maxReleaseTimeInSeconds has to be greater than 0, given: $maxReleaseTimeInSeconds"
+        }
+        require(poolEverySecond > 0) {
+            "poolEverySecond has to be greater than 0, given: $poolEverySecond"
+        }
+    }
+
+    fun release(jobName: String, releaseVersion: String, nextDevVersion: String) {
+        val buildNumber = triggerBuild(jobName, releaseVersion, nextDevVersion)
+        pollForCompletion(jobName, buildNumber)
+    }
+
+    private fun triggerBuild(jobName: String, releaseVersion: String, nextDevVersion: String): Int {
+        var count = 0
+        lateinit var response: Response
+        do {
+            check(count < maxTriggerTries) {
+                "Cannot trigger the build, response was not successful after $maxTriggerTries attempts." +
+                    "\nJob: $jobName" +
+                    "\nResponse: $response"
+            }
+            val postUrl = createUrl("${jobUrl(jobName)}/m2release/submit")
+            val inputData = createInputData(releaseVersion, nextDevVersion)
+            val body = RequestBody.create(FORM_URLENCODED, inputData)
+            val request = Request.Builder()
+                .url(postUrl)
+                .addBasicAuthHeader()
+                .post(body)
+                .build()
+            response = httpClient.newCall(request).execute()
+            ++count
+        } while (!response.isSuccessful)
+
+        // We somehow have to get the build number.
+        // Unfortunately it is not returned by jenkins that's why we need to extract it from the resulting HTML
+        return extractBuildNumber(response, jobName)
+    }
+
+    private fun createUrl(urlSpec: String): URL {
+        try {
+            return URL(urlSpec)
+        } catch (e: MalformedURLException) {
+            throw IllegalArgumentException(
+                "Cannot create a POST-URL. Most probably there is an error in the given jenkinsBaseUrl." +
+                    "\nUrl: $urlSpec", e
+            )
+        }
+    }
+
+    private fun jobUrl(jobName: String) = "$jenkinsBaseUrl/job/$jobName"
+
+    private fun Request.Builder.addBasicAuthHeader(): Request.Builder {
+        val authEncBytes = Base64.getEncoder().encode("$jenkinsUsername:$jenkinsPassword".toByteArray())
+        return header("Authorization", "Basic ${String(authEncBytes)}")
+    }
+
+    private fun createInputData(releaseVersion: String, nextDevVersion: String): String {
+        return "releaseVersion=$releaseVersion" +
+            "&developmentVersion=$nextDevVersion" +
+            createJson()
+    }
+
+    private fun createJson(): String {
+        val sb = StringBuilder("&json={\"isDryRun\":false")
+        val itr = parameters.entries.iterator()
+        if (itr.hasNext()) {
+            sb.append(",\"parameter\":[")
+            sb.appendParameter(itr.next())
+            while (itr.hasNext()) {
+                sb.append(",")
+                sb.appendParameter(itr.next())
+            }
+            sb.append("]")
+        }
+        sb.append("}")
+        return sb.toString()
+    }
+
+    private fun StringBuilder.appendParameter(entry: Map.Entry<String, String>) {
+        append("{")
+            .append("\"name\":\"").append(entry.key).append("\",")
+            .append("\"value\":\"").append(entry.value).append("\"")
+        append("}")
+    }
+
+    private fun extractBuildNumber(response: Response, jobName: String): Int {
+        val body = response.body()
+        check(body != null) {
+            "body of the response was null, cannot extract the build number." +
+                "\nJob: $jobName" +
+                "\nResponse: $response"
+        }
+        val content = body!!.string()
+        val matchResult = builderNumberRegex.find(content)
+        if (matchResult != null) {
+            return matchResult.groupValues[1].toInt()
+        }
+        throw IllegalStateException(
+            "Could not find the build number in the returned body." +
+                "\nJob: $jobName" +
+                "\nRegex used: ${builderNumberRegex.pattern}" +
+                "\n50 first chars of the body: ${content.take(50)}"
+        )
+    }
+
+    private fun pollForCompletion(jobName: String, buildNumber: Int) {
+        val url = createUrl("${jobUrl(jobName)}/$buildNumber/api/xml?xpath=/*/result")
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        var result: String?
+        var count = 0
+        val maxCount = (maxReleaseTimeInSeconds / poolEverySecond) +
+                if (maxReleaseTimeInSeconds % poolEverySecond != 0) 1 else 0
+        do {
+            check(count < maxCount) {
+                "Waited at least $maxReleaseTimeInSeconds seconds for the release to complete, aborting now." +
+                    "\nJob: $jobName"
+            }
+            Thread.sleep(poolEverySecond * 1000L)
+            val response = httpClient.newCall(request).execute()
+            result = extractResult(response, response.body())
+            ++count
+
+        } while (result == null)
+
+        check(result == "SUCCESS") {
+            "Result of the run was not SUCCESS but $result" +
+                "\nJob: $jobName"
+        }
+    }
+
+    private fun extractResult(response: Response, body: ResponseBody?): String? {
+        if (response.isSuccessful && body != null) {
+            val matchResult = resultRegex.matchEntire(body.string())
+            if (matchResult != null) {
+                return matchResult.groupValues[1]
+            }
+        }
+        return null
+    }
+
+    companion object {
+        private val FORM_URLENCODED = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8")
+        private val builderNumberRegex = Regex(
+            "<div[^>]+id=\"buildHistoryPage\"[^>]*>[\\S\\s]*?" +
+                "<td[^>]+class=\"build-row-cell[^>]+>[\\S\\s]*?" +
+                "<a[^>]+href=\"[^\"]*/job/[^/]+/([0-9]+)/console\"[^>]*>[\\S\\s]*?" +
+                "<img[^>]+src=\"[^\"]*/plugin/m2release[^>]+>[\\S\\s]*?" +
+                "</td>"
+        )
+        private val resultRegex = Regex("<result>([A-Z]+)</result>")
+    }
+}
---
+++ dep-graph-releaser-jenkins/src/test/kotlin/ch/loewenfels/depgraph/jenkins/RemoteJenkinsM2ReleaserSpec.kt
@@ -0,0 +1,234 @@
+package ch.loewenfels.depgraph.jenkins
+
+import ch.tutteli.atrium.api.cc.en_UK.contains
+import ch.tutteli.atrium.api.cc.en_UK.message
+import ch.tutteli.atrium.api.cc.en_UK.toThrow
+import ch.tutteli.atrium.assert
+import ch.tutteli.atrium.expect
+import com.nhaarman.mockito_kotlin.*
+import okhttp3.*
+import okio.BufferedSource
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
+import org.mockito.invocation.InvocationOnMock
+
+object RemoteJenkinsM2ReleaserSpec : Spek({
+
+    describe("misconfiguration") {
+        fun TestContainer.testMisconfiguration(
+            prop: String,
+            cause: String,
+            jenkinsBaseUrl: String,
+            jenkinsUsername: String,
+            jenkinsPassword: String,
+            maxTriggerRetries: Int,
+            maxReleaseTimeInSeconds: Int,
+            poolEverySecond: Int
+        ) {
+            it("throws an IllegalArgumentException in case the $prop $cause") {
+                expect {
+                    RemoteJenkinsM2Releaser(
+                        OkHttpClient(),
+                        jenkinsBaseUrl,
+                        jenkinsUsername,
+                        jenkinsPassword,
+                        maxTriggerRetries,
+                        maxReleaseTimeInSeconds,
+                        poolEverySecond,
+                        mapOf()
+                    )
+                }.toThrow<IllegalArgumentException> { message { contains(prop) } }
+            }
+        }
+
+        testMisconfiguration(
+            "jenkinsBaseUrl", "does not start with http",
+            "ftp://asdf", "user", "password", 1, 1, 1
+        )
+        testMisconfiguration(
+            "jenkinsUsername", "is blank",
+            "http://asdf", "", "password", 1, 1, 1
+        )
+        testMisconfiguration(
+            "jenkinsPassword", "is blank",
+            "http://asdf", "user", "", 1, 1, 1
+        )
+        testMisconfiguration(
+            "maxTriggerTries", "is less than 1",
+            "http://asdf", "user", "password", 0, 1, 1
+        )
+        testMisconfiguration(
+            "maxReleaseTimeInSeconds", "is less than 1",
+            "http://asdf", "user", "password", 1, 0, 1
+        )
+        testMisconfiguration(
+            "poolEverySecond", "is less than 1",
+            "http://asdf", "user", "password", 1, 1, 0
+        )
+    }
+
+    val jobName = "testJob"
+    describe("error cases trigger") {
+        given("response is always 500") {
+            context("maxTriggerTries 1") {
+                it("calls httpClient only once and throws an IllegalStateException") {
+                    val httpClient = createFailingHttpClient()
+                    val testee = createTestee(httpClient, 1)
+                    expect {
+                        testee.release(jobName, "1.0.0", "1.2.0")
+                    }.toThrow<IllegalStateException> {
+                        message { contains(jobName, "1 attempts") }
+                    }
+                    verify(httpClient, times(1)).newCall(any())
+                }
+            }
+            context("maxTriggerTries 2") {
+                it("calls httpClient twice and throws an IllegalStateException") {
+                    val httpClient = createFailingHttpClient()
+                    val testee = createTestee(httpClient, 2)
+                    expect {
+                        testee.release(jobName, "1.0.0", "1.2.0")
+                    }.toThrow<IllegalStateException> {
+                        message { contains(jobName, "2 attempts") }
+                    }
+                    verify(httpClient, times(2)).newCall(any())
+                }
+            }
+        }
+
+        given("response OK but body null") {
+            context("maxTriggerTries 2") {
+                it("calls httpClient only once throws an IllegalStateException") {
+                    val httpClient = createOkHttpClientWithBody(null)
+                    val testee = createTestee(httpClient, 2)
+                    expect {
+                        testee.release(jobName, "1.0.0", "1.2.0")
+                    }.toThrow<IllegalStateException> {
+                        message { contains(jobName, "body of the response was null") }
+                    }
+                    verify(httpClient, times(1)).newCall(any())
+                }
+            }
+        }
+
+        given("response OK but does not contain build number") {
+            it("throws an IllegalStateException") {
+                val response = "<html></html>"
+                val httpClient = createOkHttpClientWithBody(createBody(response))
+                val testee = createTestee(httpClient, 2)
+                expect {
+                    testee.release(jobName, "1.0.0", "1.2.0")
+                }.toThrow<IllegalStateException> {
+                    message { contains(jobName, "Could not find the build number", response) }
+                }
+            }
+        }
+    }
+
+    describe("error cases polling") {
+        given("maxReleaseTimeInSeconds is 1 second and no success after 1 seconds") {
+            it("throws an IllegalStateException, mentioning that timeout was reached") {
+                val httpClient = createOkHttpClientWithBody(
+                    createBody(createHtml(jobName, 12)),
+                    createBody("not yet available"),
+                    createBody("<result>SUCCESS</result>")
+                )
+                val testee = createTestee(httpClient, 1)
+                expect {
+                    testee.release(jobName, "1.0.0", "1.2.0")
+                }.toThrow<IllegalStateException>{message { contains(jobName, "Waited at least 1 seconds") }}
+            }
+        }
+
+        given("result is FAILURE") {
+            it("throws an IllegalStateException, mentioning the status") {
+                val httpClient = createOkHttpClientWithBody(
+                    createBody(createHtml(jobName, 12)),
+                    createBody("<result>FAILURE</result>")
+                )
+                val testee = createTestee(httpClient, 1)
+                expect {
+                    testee.release(jobName, "1.0.0", "1.2.0")
+                }.toThrow<IllegalStateException>{message { contains(jobName, "not SUCCESS but FAILURE") }}
+            }
+        }
+    }
+
+    describe("happy case") {
+        it("pools with the returned buildNumber and does not throw if result is SUCCESS") {
+            val buildNumber = 12
+            val response = createHtml(jobName, buildNumber)
+            val httpClient = createOkHttpClientWithBody(createBody(response), createBody("<result>SUCCESS</result>"))
+            val testee = createTestee(httpClient, 1)
+            testee.release(jobName, "1.0.0", "1.2.0")
+            val captor = argumentCaptor<Request>()
+            verify(httpClient, times(2)).newCall(captor.capture())
+            assert(captor.secondValue.toString()).contains("$HOST/job/$jobName/$buildNumber/api/xml?xpath=/*/result")
+        }
+    }
+})
+
+private fun createHtml(jobName: String, buildNumber: Int): String {
+    return "<div id=\"buildHistoryPage\"><tr><td class=\"build-row-cell\">" +
+        "<a href=\"jenkins/job/$jobName/$buildNumber/console\"><img src=\"asdf/plugin/m2release\">" +
+        "</td><tr>"
+}
+
+private fun createBody(response: String): ResponseBody {
+    return mock {
+        val bufferedSource = mock<BufferedSource> {
+            on { readString(any()) }.thenReturn(response)
+        }
+        on { source() }.thenReturn(bufferedSource)
+    }
+}
+
+private fun createFailingHttpClient() = createHttpClient(500, "Internal Server Error", null)
+
+private fun createOkHttpClientWithBody(body: ResponseBody?) = createHttpClient(200, "OK", body)
+private fun createOkHttpClientWithBody(vararg bodies: ResponseBody): OkHttpClient {
+    return mock {
+        val itr = bodies.iterator()
+        var stub = on { newCall(any()) }
+        do {
+            val body = itr.next()
+            stub = stub.then {
+                createCall(it, 200, "OK", body)
+            }
+        } while (itr.hasNext())
+    }
+}
+
+private fun createHttpClient(statusCode: Int, message: String, body: ResponseBody?): OkHttpClient {
+    return mock {
+        on { newCall(any()) }.then {
+            createCall(it, statusCode, message, body)
+        }
+    }
+}
+
+private fun createCall(
+    it: InvocationOnMock,
+    statusCode: Int,
+    message: String,
+    body: ResponseBody?
+): Call {
+    val request = it.arguments[0] as Request
+    val call = mock<Call> {
+        val builder = Response.Builder()
+        val response = builder
+            .request(request)
+            .protocol(Protocol.HTTP_1_0)
+            .code(statusCode)
+            .message(message)
+            .body(body)
+            .build()
+        on { execute() }.thenReturn(response)
+    }
+    return call
+}
+
+private fun createTestee(httpClient: OkHttpClient, maxTriggerTries: Int) =
+    RemoteJenkinsM2Releaser(httpClient, HOST, "user", "password", maxTriggerTries, 1, 1, mapOf())
+
+private const val HOST = "http://asdf"
