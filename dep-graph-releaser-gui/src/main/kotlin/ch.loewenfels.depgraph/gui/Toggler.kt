3c6adeea0eef34e66ea4d3a1892932643367e70b
Robert Stoll <robert.stoll@loewenfels.ch>
2018-04-11 08:52:49 +0200

    get rid of iterateCommand and use regular filter etc.

--- dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/Toggler.kt
+++ dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/Toggler.kt
@@ -2,6 +2,7 @@ package ch.loewenfels.depgraph.gui
 
 import ch.loewenfels.depgraph.data.Project
 import ch.loewenfels.depgraph.data.ProjectId
+import ch.loewenfels.depgraph.data.ReleaseCommand
 import ch.loewenfels.depgraph.data.ReleasePlan
 import org.w3c.dom.HTMLInputElement
 import kotlin.browser.document
@@ -12,77 +13,84 @@ class Toggler(private val releasePlan: ReleasePlan, private val menu: Menu) {
     fun toggle(id: String) {
         val checkbox = getCheckbox(id)
         when {
-            id.endsWith(":disableAll") -> toggleProject(checkbox, id)
-            else -> toggleCommand(checkbox, id)
+            id.endsWith(DISABLE_ALL) -> toggleProject(checkbox)
+            else -> toggleCommand(checkbox)
         }
     }
 
-    private fun toggleProject(disableAllCheckbox: HTMLInputElement, id: String) {
+    private fun toggleProject(disableAllCheckbox: HTMLInputElement) {
         val checked = disableAllCheckbox.checked
-        val prefix = id.substring(0, id.indexOf(":disableAll"))
-        iterateCommands(prefix) { checkbox, i ->
+        val identifier = disableAllCheckbox.id.substring(0, disableAllCheckbox.id.indexOf(DISABLE_ALL))
+        val project = getProject(identifier)
+
+        project.commands.forEachIndexed { index, _ ->
+            val checkbox = getCheckbox(identifier, index)
             //do nothing if command is disabled
-            if (checkbox.disabled) return@iterateCommands
+            if (checkbox.disabled) return@forEachIndexed
 
             menu.activateSaveButton()
             checkbox.checked = checked
             if (checkbox.isReleaseCommand() && !checked) {
-                toggleCommand(checkbox, "$prefix:$i:disable")
+                toggleCommand(checkbox)
             }
         }
     }
 
-    private fun toggleCommand(checkbox: HTMLInputElement, id: String) {
-        val regex = Regex("(.*):[0-9]+:disable")
-        val result = regex.find(id)
-        require(result != null) {
-            "the given id ($id) did not match the pattern $regex"
-        }
-        val prefix = result!!.groups[1]!!.value
+    private fun getDisableAllId(projectId: ProjectId) = "${projectId.identifier}:$DISABLE_ALL"
+    private fun getCheckbox(identifier: String, index: Int) = getCheckbox("$identifier:$index:disable")
+
+    private fun toggleCommand(checkbox: HTMLInputElement) {
+        val project = getProjectOfCommand(checkbox)
         if (!checkbox.checked) {
             menu.activateSaveButton()
-            deactivateReleaseCommands(prefix, id)
-            deactivateDependents(prefix)
+            deactivateReleaseCommands(project, checkbox.id)
+            deactivateDependents(project.id)
         } else if (checkbox.isReleaseCommand()) {
-            if (notAllCommandsActive(prefix, id) || notAllSubmodulesActive(prefix)) {
+            if (notAllCommandsActive(project, { it.id != checkbox.id }) || notAllSubmodulesActive(project)) {
                 // cannot reactivate release command if not all commands are active
                 // setting checked again to false
                 checkbox.checked = false
-                showInfo("Cannot reactivate the ReleaseCommand for project $prefix because some commands (of submodules) are deactivated.")
+                showInfo("Cannot reactivate the ReleaseCommand for project ${project.id.identifier} because some commands (of submodules) are deactivated.")
             }
             menu.activateSaveButton()
         }
     }
 
-    private fun deactivateReleaseCommands(prefix: String, id: String) {
-        iterateCommands(prefix) { checkbox, _ ->
-            if (checkbox.id != id && checkbox.isReleaseCommand()) {
-                checkbox.checked = false
-            }
+    private fun getProjectOfCommand(checkbox: HTMLInputElement): Project {
+        val regex = Regex("(.*):[0-9]+:disable")
+        val result = regex.find(checkbox.id)
+        require(result != null) {
+            "the given id ($checkbox.id) did not match the pattern $regex"
         }
+        val identifier = result!!.groups[1]!!.value
+        return getProject(identifier)
     }
 
-    private fun deactivateDependents(prefix: String) {
-        val project = getProject(prefix)
-        deactivateDependents(project.id)
-    }
+    private fun getProject(identifier: String) = elementById(identifier).asDynamic().project as Project
 
-    private fun getProject(prefix: String) = elementById(prefix).asDynamic().project as Project
+    private fun deactivateReleaseCommands(project: Project, id: String) {
+        project.commands.asSequence()
+            .mapIndexed { index, it -> index to it }
+            .filter { it.second is ReleaseCommand }
+            .map { (index, _) -> getCheckbox(project.id.identifier, index) }
+            .filter { checkbox -> checkbox.id != id }
+            .forEach { checkbox -> checkbox.checked = false }
+    }
 
     private fun deactivateDependents(projectId: ProjectId) {
         releasePlan.getDependents(projectId).forEach(this::disableProject)
-        releasePlan.getSubmodules(projectId).forEach {
-            val id = "${it.identifier}:disableAll"
+        releasePlan.getSubmodules(projectId).forEach { submoduleId ->
+            val id = getDisableAllId(submoduleId)
             if (document.getElementById(id) != null) {
-                disableProject(it)
+                disableProject(submoduleId)
             } else {
-                deactivateDependents(it)
+                deactivateDependents(submoduleId)
             }
         }
     }
 
-    private fun disableProject(it: ProjectId) {
-        val id = "${it.identifier}:disableAll"
+    private fun disableProject(projectId: ProjectId) {
+        val id = getDisableAllId(projectId)
         val checkbox = document.getElementById(id)
         //could be a sub module without commands
         if (checkbox is HTMLInputElement) {
@@ -95,40 +103,23 @@ class Toggler(private val releasePlan: ReleasePlan, private val menu: Menu) {
         return classList.contains("release")
     }
 
-    private fun notAllCommandsActive(prefix: String, id: String): Boolean {
-        var notAllChecked = false
-        iterateCommands(prefix) { checkbox, _ ->
-            if (checkbox.id != id && !checkbox.checked) {
-                notAllChecked = true
-            }
+    private fun notAllCommandsActive(project: Project, predicate: (HTMLInputElement) -> Boolean): Boolean {
+        return project.commands.asSequence()
+            .mapIndexed { index, _ -> getCheckbox(project.id.identifier, index) }
+            .filter(predicate)
+            .any { checkbox -> !checkbox.checked }
+    }
+
+    private fun notAllSubmodulesActive(project: Project): Boolean {
+        return releasePlan.getSubmodules(project.id).any { submoduleId ->
+            val submodulesHasCommands = document.getElementById(getDisableAllId(project.id)) != null
+            submodulesHasCommands &&
+                //cannot be the same command, hence we do not filter commands at all => use `{ true }`
+                notAllCommandsActive(releasePlan.getProject(submoduleId), { true })
         }
-        return notAllChecked
     }
 
-    private fun notAllSubmodulesActive(prefix: String): Boolean {
-        val project = getProject(prefix)
-        releasePlan.getSubmodules(project.id).forEach {
-            val id = "${it.identifier}:disableAll"
-            if (document.getElementById(id) != null) {
-                if (notAllCommandsActive(it.identifier, "notACommandOfTheSubmodule")) {
-                    return true
-                }
-            }
-        }
-        return false
+    companion object {
+        private const val DISABLE_ALL = ":disableAll"
     }
-
-    private fun iterateCommands(prefix: String, act: (HTMLInputElement, Int) -> Unit) {
-        var i = 0
-        do {
-            val checkbox = getCheckboxOrNull("$prefix:$i:disable")
-            val found = checkbox != null
-            if (found) {
-                act(checkbox!!, i)
-                ++i
-            }
-        } while (found)
-    }
-
-
 }
