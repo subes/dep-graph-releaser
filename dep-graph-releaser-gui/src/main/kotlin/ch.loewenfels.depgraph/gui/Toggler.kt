77b5bec30fb8238c2a92160e660071b9c463c87e
Robert Stoll <robert.stoll@loewenfels.ch>
2018-04-19 16:16:33 +0200

    switch to event based toggling

--- dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/Toggler.kt
+++ dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/Toggler.kt
@@ -10,6 +10,7 @@ import org.w3c.dom.CustomEventInit
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.Event
 import kotlin.browser.document
+import kotlin.js.Promise
 
 class Toggler(private val releasePlan: ReleasePlan, private val menu: Menu) {
 
@@ -17,154 +18,123 @@ class Toggler(private val releasePlan: ReleasePlan, private val menu: Menu) {
         releasePlan.getProjects().forEach { project ->
             val allToggle = getAllToggle(project) ?: return@forEach
 
-            allToggle.addChangeEventListener { toggle(allToggle.id) }
-            project.commands.forEachIndexed { index, command ->
-                val toggle = getToggle(project, index)
-                toggle.addChangeEventListener { toggleCommand(project, toggle) }
-                disableUnDisableForReleaseStartAndEnd(toggle, elementById("${toggle.id}:slider"))
-                if (command is ReleaseCommand) {
-                    registerReleaseCommandEvents(toggle, project)
-                }
+            registerAllToggleEvents(allToggle, project)
+            registerCommandToggleEvents(project)
+            registerReleaseUncheckEventForDependentsAndSubmodules(project)
+        }
+    }
+
+    private fun getAllToggle(project: Project): HTMLInputElement? = getAllToggle(project.id)
+    private fun getAllToggle(projectId: ProjectId): HTMLInputElement? =
+        elementByIdOrNull(projectId.identifier + Gui.DISABLE_ALL_SUFFIX)
+
+    private fun registerAllToggleEvents(allToggle: HTMLInputElement, project: Project) {
+        allToggle.addChangeEventListener {
+            val event = if (allToggle.checked) EVENT_ALL_TOGGLE_CHECKED else EVENT_ALL_TOGGLE_UNCHECKED
+            dispatchToggleEvent(project, allToggle, event)
+        }
+    }
+
+    private fun registerCommandToggleEvents(project: Project) {
+        project.commands.forEachIndexed { index, command ->
+            val toggle = getToggle(project, index)
+
+            if (command is ReleaseCommand) {
+                toggle.addChangeEventListener { toggleCommand(project, toggle, EVENT_RELEASE_TOGGLE_UNCHECKED) }
+                disallowClickIfNotAllCommandsOrSubmodulesActive(project, toggle)
+                registerForProjectEvent(project, EVENT_TOGGLE_UNCHECKED) { toggle.uncheck() }
+            } else {
+                toggle.addChangeEventListener { toggleCommand(project, toggle, EVENT_TOGGLE_UNCHECKED) }
+            }
+
+            disableUnDisableForReleaseStartAndEnd(toggle, elementById("${toggle.id}:slider"))
+            registerForProjectEvent(project, EVENT_ALL_TOGGLE_CHECKED) {
+                toggle.check()
+            }
+            registerForProjectEvent(project, EVENT_ALL_TOGGLE_UNCHECKED) {
+                toggle.uncheck()
             }
         }
     }
 
-    private fun registerReleaseCommandEvents(toggle: HTMLInputElement, project: Project) {
+    private fun getToggle(project: Project, index: Int): HTMLInputElement =
+        elementById<HTMLInputElement>(Gui.getCommandId(project, index) + Gui.DISABLE_SUFFIX)
+
+    private fun toggleCommand(project: Project, toggle: HTMLInputElement, event: String) {
+        if (!toggle.checked) {
+            dispatchToggleEvent(project, toggle, event)
+        }
+        menu.activateSaveButton()
+    }
+
+    private fun disallowClickIfNotAllCommandsOrSubmodulesActive(project: Project, toggle: HTMLInputElement) {
         toggle.addClickEventListener { e ->
             if (toggle.checked && notAllCommandsOrSubmodulesActive(project, toggle)) {
                 // cannot reactivate release command if not all commands are active
                 // setting checked again to false
                 //toggle.checked = false
                 e.preventDefault()
-                showInfo("Cannot reactivate the ReleaseCommand for project ${project.id.identifier} because some commands (of submodules) are deactivated.", 5000)
+                showInfo(
+                    "Cannot reactivate the ReleaseCommand for project ${project.id.identifier} " +
+                        "because some commands (of submodules) are deactivated.",
+                    4000
+                )
             }
         }
-        registerForProjectEvent(project, EVENT_TOGGLE_UNCHECKED) {
-            if (toggle.checked) {
-                toggle.checked = false
-            }
-        }
-    }
-
-    private fun getAllToggle(project: Project): HTMLInputElement? =
-        elementByIdOrNull(project.id.identifier + Gui.DISABLE_ALL_SUFFIX)
-
-    private fun getToggle(project: Project, index: Int): HTMLInputElement =
-        elementById<HTMLInputElement>(Gui.getCommandId(project, index) + Gui.DISABLE_SUFFIX)
-
-    private fun toggleCommand(project: Project, toggle: HTMLInputElement) {
-        if (!toggle.checked) {
-            dispatchToggleEvent(project, toggle, EVENT_TOGGLE_UNCHECKED)
-            toggle(toggle.id)
-        } else {
-            toggle(toggle.id)
-        }
-        menu.activateSaveButton()
     }
 
     private fun notAllCommandsOrSubmodulesActive(project: Project, toggle: HTMLInputElement): Boolean {
         return notAllCommandsActive(project, { it.id != toggle.id }) || notAllSubmodulesActive(project)
     }
 
-    private fun dispatchToggleEvent(project: Project, toggle: HTMLInputElement, event: String) {
-        projectElement(project).dispatchEvent(CustomEvent(event, CustomEventInit(toggle)))
+    private fun registerReleaseUncheckEventForDependentsAndSubmodules(project: Project) {
+        if (!project.isSubmodule) {
+            val projectIds = collectDependentsOnNextLevelAndSubmodulesAndTheirDependents(project)
+
+            projectIds.forEach { projectId ->
+                registerForProjectEvent(project, EVENT_RELEASE_TOGGLE_UNCHECKED) {
+                    getAllToggle(projectId)?.uncheck()
+                }
+            }
+        }
     }
 
-    private fun registerForProjectEvent(project: Project, event: String, callback: (Event) -> Unit) {
-        projectElement(project).addEventListener(event, callback)
+    private fun collectDependentsOnNextLevelAndSubmodulesAndTheirDependents(project: Project): HashSet<ProjectId> {
+        val projectIds = hashSetOf<ProjectId>()
+        val projectsToVisit = mutableListOf(project.id)
+        do {
+            val projectId = projectsToVisit.removeAt(0)
+            projectIds.addAll(getDependentsOnNextLevel(project, projectId))
+            val submodules = releasePlan.getSubmodules(projectId)
+            projectIds.addAll(submodules)
+            projectsToVisit.addAll(submodules)
+        } while(projectsToVisit.isNotEmpty())
+        return projectIds
     }
+
+    private fun getDependentsOnNextLevel(mainProject: Project, projectId: ProjectId) =
+        releasePlan.getDependents(projectId).filter { mainProject.level + 1 == releasePlan.getProject(it).level }
+
+
+    private fun HTMLInputElement.uncheck() = changeChecked(this, false)
+    private fun HTMLInputElement.check() = changeChecked(this, true)
+
+    private fun changeChecked(toggle: HTMLInputElement, checked: Boolean) {
+        if (toggle.checked == checked) return
+
+        toggle.checked = checked
+        Promise.resolve(0).then { //used to avoid stack-overflow
+            toggle.dispatchEvent(Event("change"))
+        }
+    }
+
 
     private fun projectElement(project: Project) = elementById(project.id.identifier)
 
-    @JsName("toggle")
-    fun toggle(id: String) {
-        val checkbox = getCheckbox(id)
-        when {
-            id.endsWith(Gui.DISABLE_ALL_SUFFIX) -> toggleProject(checkbox)
-            else -> toggleCommand(checkbox)
-        }
-    }
-
-    private fun toggleProject(disableAllCheckbox: HTMLInputElement) {
-        val checked = disableAllCheckbox.checked
-        val identifier = disableAllCheckbox.id.substring(0, disableAllCheckbox.id.indexOf(Gui.DISABLE_ALL_SUFFIX))
-        val project = getProject(identifier)
-
-        project.commands.forEachIndexed { index, _ ->
-            val checkbox = getCheckbox(identifier, index)
-            //do nothing if command is disabled
-            if (checkbox.disabled) return@forEachIndexed
-
-            menu.activateSaveButton()
-            checkbox.checked = checked
-        }
-        releasePlan.getSubmodules(project.id)
-            .asSequence()
-            .map { submoduleId -> getCheckboxOrNull(getDisableAllId(submoduleId)) }
-            .filterNotNull()
-            .forEach { checkbox ->
-                checkbox.checked = checked
-                toggleProject(checkbox)
-            }
-    }
 
     private fun getDisableAllId(projectId: ProjectId) = "${projectId.identifier}${Gui.DISABLE_ALL_SUFFIX}"
     private fun getCheckbox(identifier: String, index: Int) = getCheckbox("$identifier:$index${Gui.DISABLE_SUFFIX}")
 
-    private fun toggleCommand(checkbox: HTMLInputElement) {
-        val project = getProjectOfCommand(checkbox)
-        if (!checkbox.checked) {
-            menu.activateSaveButton()
-            deactivateDependents(project.id)
-        } else if (checkbox.isReleaseCommand()) {
-            if (notAllCommandsActive(project, { it.id != checkbox.id }) || notAllSubmodulesActive(project)) {
-                // cannot reactivate release command if not all commands are active
-                // setting checked again to false
-                checkbox.checked = false
-                showInfo("Cannot reactivate the ReleaseCommand for project ${project.id.identifier} because some commands (of submodules) are deactivated.")
-            }
-            menu.activateSaveButton()
-        }
-    }
-
-    private fun getProjectOfCommand(checkbox: HTMLInputElement): Project {
-        val regex = Regex("(.*):[0-9]+${Gui.DISABLE_SUFFIX}")
-        val result = regex.find(checkbox.id)
-        require(result != null) {
-            "the given id ($checkbox.id) did not match the pattern $regex"
-        }
-        val identifier = result!!.groups[1]!!.value
-        return getProject(identifier)
-    }
-
-    private fun getProject(identifier: String) = elementById(identifier).asDynamic().project as Project
-
-
-    private fun deactivateDependents(projectId: ProjectId) {
-        releasePlan.getDependents(projectId).forEach(this::disableProject)
-        releasePlan.getSubmodules(projectId).forEach { submoduleId ->
-            val id = getDisableAllId(submoduleId)
-            if (document.getElementById(id) != null) {
-                disableProject(submoduleId)
-            } else {
-                deactivateDependents(submoduleId)
-            }
-        }
-    }
-
-    private fun disableProject(projectId: ProjectId) {
-        val id = getDisableAllId(projectId)
-        val checkbox = document.getElementById(id)
-        //could be a sub module without commands
-        if (checkbox is HTMLInputElement) {
-            checkbox.checked = false
-            toggle(id)
-        }
-    }
-
-    private fun HTMLInputElement.isReleaseCommand(): Boolean {
-        return classList.contains("release")
-    }
 
     private fun notAllCommandsActive(project: Project, predicate: (HTMLInputElement) -> Boolean): Boolean {
         return project.commands.asSequence()
@@ -182,7 +152,18 @@ class Toggler(private val releasePlan: ReleasePlan, private val menu: Menu) {
         }
     }
 
+    private fun dispatchToggleEvent(project: Project, toggle: HTMLInputElement, event: String) {
+        projectElement(project).dispatchEvent(CustomEvent(event, CustomEventInit(toggle)))
+    }
+
+    private fun registerForProjectEvent(project: Project, event: String, callback: (Event) -> Unit) {
+        projectElement(project).addEventListener(event, callback)
+    }
+
     companion object {
         private const val EVENT_TOGGLE_UNCHECKED = "toggle.unchecked"
+        private const val EVENT_RELEASE_TOGGLE_UNCHECKED = "release.toggle.unchecked"
+        private const val EVENT_ALL_TOGGLE_CHECKED = "all.toggle.checked"
+        private const val EVENT_ALL_TOGGLE_UNCHECKED = "all.toggle.unchecked"
     }
 }
