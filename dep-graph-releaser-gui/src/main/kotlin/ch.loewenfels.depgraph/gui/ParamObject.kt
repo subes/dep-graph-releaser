bafaac148e6f76ec2bd5b985f7bb6107282f7ae7
Robert Stoll <robert.stoll@loewenfels.ch>
2018-05-16 14:48:56 +0200

    remove file ParamObject it is useless

--- dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/ParamObject.kt
+++
@@ -1,8 +0,0 @@
-package ch.loewenfels.depgraph.gui
-
-import ch.loewenfels.depgraph.data.CommandState
-import ch.loewenfels.depgraph.data.Project
-import ch.loewenfels.depgraph.data.ProjectId
-import ch.loewenfels.depgraph.data.ReleasePlan
-import kotlin.js.Promise
-
