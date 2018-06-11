f8dd217b2afbdc246c9e08f13a4d1f24d0549838
Robert Stoll <robert.stoll@loewenfels.ch>
2018-06-11 15:55:44 +0200

    fix modal positioning, ref should be window height not body height
    Moreover:
    - take box height into account when positioning
    - reduce space between icon and title in output modal

--- dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/popups.kt
+++ dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/popups.kt
@@ -160,8 +160,10 @@ fun showOutput(title: String, output: String): Promise<Unit> {
     return Promise { resolve, _ ->
         showModal(
             {
-                i("material-icons") { +"list_alt" }; span("title") { +title }
-                textArea { +output }
+                div("output") {
+                    i("material-icons") { +"list_alt" }; span { +title }
+                    textArea { +output }
+                }
             },
             { box -> modalButton("OK", box, resolve, Unit) }
         )
@@ -201,8 +203,8 @@ private fun showModal(
         }
     }
     val box = modals.lastChild as HTMLElement
-    val top = document.body!!.clientHeight / 3
-    val left = document.body!!.clientWidth / 2 - box.offsetWidth / 2
+    val top = window.innerHeight / 2.5 - box.offsetHeight / 2
+    val left = window.innerWidth / 2 - box.offsetWidth / 2
     box.style.top = "${top}px"
     box.style.left = "${left}px"
     box.style.visibility = "visible"
