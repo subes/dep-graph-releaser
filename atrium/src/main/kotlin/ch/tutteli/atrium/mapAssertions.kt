f96b36e48dd51eb7e7d3d1c2c89f0b71b9403426
Robert Stoll <robert.stoll@loewenfels.ch>
2018-06-16 16:27:26 +0200

    use isEmpty and hasSize for Map from Atrium

--- atrium/src/main/kotlin/ch/tutteli/atrium/mapAssertions.kt
+++ atrium/src/main/kotlin/ch/tutteli/atrium/mapAssertions.kt
@@ -4,15 +4,6 @@ import ch.tutteli.atrium.api.cc.en_GB.property
 import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.creating.Assert
 
-//TODO replace with isEmpty from Atrium as soon as 0.7.0 is out
-fun <K, V> Assert<Map<K, V>>.isEmpty() = hasSize(0)
-
-//TODO replace with hasSize from Atrium as soon as 0.7.0 is out
-fun <K, V> Assert<Map<K, V>>.hasSize(size: Int): Assert<Map<K, V>> {
-    property(subject::size).toBe(size)
-    return this
-}
-
 //TODO replace with keyValue from Atrium as soon as 0.8.0 is out
 fun <K : Any, V : Any> Assert<Map.Entry<K, V>>.keyValue(k: K, v: V): Assert<Map.Entry<K, V>> {
     property(subject::key).toBe(k)
