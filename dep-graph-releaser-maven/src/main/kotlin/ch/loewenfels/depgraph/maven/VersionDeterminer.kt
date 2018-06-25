21698045957a9f4ec0136ac3473ae7a5c890f0ca
eme01 <mario.eberl@loewenfels.ch>
2018-06-25 23:17:54 +0200

    Make version determination more abstract
    - remove concepts based on major/minor/patch versioning scheme
    - simplify determination of release version and development version, as
      they both build upon the same logic

--- dep-graph-releaser-maven/src/main/kotlin/ch/loewenfels/depgraph/maven/VersionDeterminer.kt
+++ dep-graph-releaser-maven/src/main/kotlin/ch/loewenfels/depgraph/maven/VersionDeterminer.kt
@@ -9,10 +9,16 @@ class VersionDeterminer {
         else -> updateLastNumber(currentVersion)
     }
 
+    fun nextDevVersion(currentVersion: String): String {
+        val releaseVersion = releaseVersion(currentVersion)
+        return updateLastNumber(releaseVersion) + SNAPSHOT_SUFFIX
+    }
+
     private fun updateLastNumber(version: String): String {
         val matcher = LAST_NUMBER_PATTERN.matcher(version)
         return if (matcher.find()) {
-            matcher.group(1) + incrementNumber(matcher, 2)
+            val incrementedNumber = incrementNumber(matcher, 2)
+            matcher.replaceFirst("$1$incrementedNumber")
         } else {
             "$version.2"
         }
@@ -20,29 +26,8 @@ class VersionDeterminer {
 
     private fun incrementNumber(matcher: Matcher, index: Int): Int = matcher.group(index).toInt() + 1
 
-    fun nextDevVersion(currentVersion: String): String {
-        val releaseVersion = releaseVersion(currentVersion)
-        val matcher = MAJOR_MINOR_PATCH_PATTERN.matcher(releaseVersion)
-        matcher.find()
-        val incrementedLastDigit = incrementNumber(matcher, PATCH_GROUP).toString()
-        return optionalPrefix(matcher) + when {
-            onlyMajor(matcher) -> incrementedLastDigit + SNAPSHOT_SUFFIX
-            onlyMajorAndMinor(matcher) -> matcher.group(MAJOR_GROUP) + incrementedLastDigit + SNAPSHOT_SUFFIX
-            else -> matcher.group(MAJOR_GROUP) + matcher.group(MINOR_GROUP) + incrementNumber(matcher, PATCH_GROUP) + SNAPSHOT_SUFFIX
-        }
-    }
-
-    private fun onlyMajor(matcher: Matcher) = matcher.group(MAJOR_GROUP) == null
-    private fun onlyMajorAndMinor(matcher: Matcher) = matcher.group(MINOR_GROUP) == null
-    private fun optionalPrefix(matcher: Matcher): String = matcher.group(PREFIX_GROUP) ?: ""
-
     companion object {
         private const val SNAPSHOT_SUFFIX = "-SNAPSHOT"
-        private val LAST_NUMBER_PATTERN = Pattern.compile("(\\D*(?:\\d+\\.)*)(\\d+)\\D*")
-        private val MAJOR_MINOR_PATCH_PATTERN = Pattern.compile("(\\D*)(\\d+\\D+)?(\\d+\\D+)?(\\d+)")
-        private const val PREFIX_GROUP = 1
-        private const val MAJOR_GROUP = 2
-        private const val MINOR_GROUP = 3
-        private const val PATCH_GROUP = 4
+        private val LAST_NUMBER_PATTERN = Pattern.compile("(.*\\.)?(\\d+)(.*)")
     }
 }
