fa0f7d581cc8579abbc4cc4584d48d4b03d5566e
Adrian Scherer <scherer.adi@gmail.com>
2019-08-29 01:36:58 +0200

    Try another regex

--- dep-graph-releaser-runner/src/test/kotlin/ch/loewenfels/depgraph/runner/console/JsonSpec.kt
+++ dep-graph-releaser-runner/src/test/kotlin/ch/loewenfels/depgraph/runner/console/JsonSpec.kt
@@ -97,7 +97,7 @@ class JsonSpec : Spek({
 
         describe("non-existing directory") {
             it("throws IllegalStateException mentioning that the directory does not exist") {
-                val happyArgs = createArgs(tempFolder, "com:a;com:b", ".#http://")
+                val happyArgs = createArgs(tempFolder, "com.example:a;com.example:b", ".*#maven#rel;nextDev;add", "")
                 val badArgs = happyArgs.asSequence().map { it.replace("managingVersions/inDependency", "managingVersions/nonExistingFolder") }.toList().toTypedArray()
                 badArgs.forEach { println(it) }
                 expect {
