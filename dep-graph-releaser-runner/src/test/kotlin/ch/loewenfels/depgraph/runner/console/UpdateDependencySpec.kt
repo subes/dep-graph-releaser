affcbf39d36e300a717a72e5ca11a1fb16f7600f
Adrian Scherer <scherer.adi@gmail.com>
2019-08-28 20:26:09 +0200

    Add spec for non-existing pom
    Add missing spec "non-existing pom file" for UpdateDependency console
    command
    
    - resolves #9

--- dep-graph-releaser-runner/src/test/kotlin/ch/loewenfels/depgraph/runner/console/UpdateDependencySpec.kt
+++ dep-graph-releaser-runner/src/test/kotlin/ch/loewenfels/depgraph/runner/console/UpdateDependencySpec.kt
@@ -2,17 +2,38 @@ package ch.loewenfels.depgraph.runner.console
 
 import ch.loewenfels.depgraph.maven.getTestDirectory
 import ch.loewenfels.depgraph.runner.commands.UpdateDependency
+import ch.tutteli.atrium.api.cc.en_GB.contains
+import ch.tutteli.atrium.api.cc.en_GB.message
+import ch.tutteli.atrium.api.cc.en_GB.toThrow
 import ch.tutteli.atrium.copyPom
+import ch.tutteli.atrium.expect
 import ch.tutteli.niok.absolutePathAsString
 import ch.tutteli.spek.extensions.TempFolder
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.include
 
 class UpdateDependencySpec : Spek({
     include(UpdateDependencyCommandSpec)
 
-    //TODO write spec for non-existing pom
-    //given("non-existing pom"){}
+    given("non-existing pom") {
+        val errMsg = "The given pom file does not exist."
+        val inputArgs = arrayOf(
+            UpdateDependency.name,
+            "non_existing_path/pom.xml",
+            "junit",
+            "junit",
+            "4.12"
+        )
+        it("throws an error, mentioning $errMsg") {
+            expect {
+                UpdateDependency.execute(inputArgs, errorHandler)
+            }.toThrow<IllegalStateException> { message { contains(errMsg) } }
+        }
+    }
+
+
 
 }) {
     object UpdateDependencyCommandSpec : CommandSpec(
